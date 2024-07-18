269927e26ad60b2d31a08558004b090d348d2e0c
RushikeshBhagwatkar <92802693+RushikeshBhagwatkar@users.noreply.github.com>
2024-07-18 20:13:30 +0530

    Update Manager.jsx

--- src/components/Manager.jsx
+++ src/components/Manager.jsx
@@ -71,7 +71,7 @@ const Manager = () => {
       <div className="absolute top-0 -z-10 h-full w-full bg-green-50">
         <div className="absolute bottom-auto left-auto right-0 top-0 h-[500px] w-[500px] -translate-x-[30%] translate-y-[20%] rounded-full bg-[rgba(173,109,244,0.5)] opacity-50 blur-[80px]"></div>
       </div>
-      <div className=" mycontainer">
+      <div className=" mycontainer w-3/4">
         <h1 className="text-4xl font-bold text-center">
           <span className="text-green-700">&lt;</span>
           Pass
